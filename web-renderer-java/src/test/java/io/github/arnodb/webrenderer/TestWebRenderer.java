f0c114ca66d7109b80976a6ab936106c2964b305
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2015-10-21 23:34:37 +0200

    More test coverage

--- web-renderer-java/src/test/java/io/github/arnodb/webrenderer/TestWebRenderer.java
+++ web-renderer-java/src/test/java/io/github/arnodb/webrenderer/TestWebRenderer.java
@@ -1,10 +1,12 @@
 package io.github.arnodb.webrenderer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
@@ -28,18 +30,7 @@ public class TestWebRenderer {
         return html.toString();
     }
 
-    @Test
-    public void test1() throws Exception {
-        Class<?> driverClass = Class.forName(DRIVER_CLASS_NAME);
-        WebDriver driver = (WebDriver) driverClass.newInstance();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        String url = "data:text/html;base64," + new String(Base64.encodeBase64(loadHtml().getBytes("UTF-8")));
-        WebRenderer.urlToPngStream(driver, url, baos);
-        driver.quit();
-
-        InputStream bais = new ByteArrayInputStream(baos.toByteArray());
-        BufferedImage image = ImageIO.read(bais);
-
+    private void assertBlueBox(BufferedImage image) {
         int[][] blues = new int[][] {{0, 0}, {3, 3}, {46, 46}, {49, 49}};
         for (int[] coord : blues) {
             int rgb = image.getRGB(coord[0], coord[1]);
@@ -51,6 +42,77 @@ public class TestWebRenderer {
             int rgb = image.getRGB(coord[0], coord[1]);
             assertEquals("rgb(" + coord[0] + ", " + coord[1] + ") is blue", "ffffffff", Integer.toHexString(rgb));
         }
+    }
+
+    @Test
+    public void testCommandLine() throws Exception {
+        File output = new File("target/test-resources/testCommandLine.png");
+        if (output.exists())
+            output.delete();
+        File parent = output.getParentFile();
+        if (!parent.exists())
+            parent.mkdirs();
+
+        String url = "data:text/html;base64," + new String(Base64.encodeBase64(loadHtml().getBytes("UTF-8")));
+        WebRenderer.main(new String[]{ "-o", output.getPath(), url });
+        assertTrue("output file '" + output + "' exists", output.exists());
+
+        BufferedImage image = ImageIO.read(output);
+        assertBlueBox(image);
+    }
+
+    @Test
+    public void testUrlToPngFileFile() throws Exception {
+        File output = new File("target/test-resources/testUrlToPngFileFile.png");
+        if (output.exists())
+            output.delete();
+        File parent = output.getParentFile();
+        if (!parent.exists())
+            parent.mkdirs();
+
+        Class<?> driverClass = Class.forName(DRIVER_CLASS_NAME);
+        WebDriver driver = (WebDriver) driverClass.newInstance();
+        String url = "data:text/html;base64," + new String(Base64.encodeBase64(loadHtml().getBytes("UTF-8")));
+        WebRenderer.urlToPngFile(driver, url, output);
+        driver.quit();
+        assertTrue("output file '" + output + "' exists", output.exists());
+
+        BufferedImage image = ImageIO.read(output);
+        assertBlueBox(image);
+    }
+
+    @Test
+    public void testUrlToPngFileString() throws Exception {
+        File output = new File("target/test-resources/testUrlToPngFileString.png");
+        if (output.exists())
+            output.delete();
+        File parent = output.getParentFile();
+        if (!parent.exists())
+            parent.mkdirs();
+
+        Class<?> driverClass = Class.forName(DRIVER_CLASS_NAME);
+        WebDriver driver = (WebDriver) driverClass.newInstance();
+        String url = "data:text/html;base64," + new String(Base64.encodeBase64(loadHtml().getBytes("UTF-8")));
+        WebRenderer.urlToPngFile(driver, url, output.getPath());
+        driver.quit();
+        assertTrue("output file '" + output + "' exists", output.exists());
+
+        BufferedImage image = ImageIO.read(output);
+        assertBlueBox(image);
+    }
+
+    @Test
+    public void testUrlToPngStream() throws Exception {
+        Class<?> driverClass = Class.forName(DRIVER_CLASS_NAME);
+        WebDriver driver = (WebDriver) driverClass.newInstance();
+        String url = "data:text/html;base64," + new String(Base64.encodeBase64(loadHtml().getBytes("UTF-8")));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        WebRenderer.urlToPngStream(driver, url, baos);
+        driver.quit();
+
+        InputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        BufferedImage image = ImageIO.read(bais);
+        assertBlueBox(image);
     }
 
 }
